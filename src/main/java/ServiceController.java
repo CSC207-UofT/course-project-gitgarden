8cf6a7eaa5431bf77299b150cd02fd2debb50e2c
hMarc16 <mark.hu@mail.utoronto.ca>
2021-10-12 15:02:07 -0400

    Moved ServiceController

---
+++ src/main/java/ServiceController.java
@@ -0,0 +1,15 @@
+
+
+/** ServiceController
+ * Responsibilities:
+ *      Accept or decline inputs from UserInterface
+ *      Make variables for ProfileManager and MatchSystem if the input is acceptable
+ *      Report exception if the input is unacceptable
+ *
+ * Collaborators:
+ *      UserInterface
+ *      ProfileManager
+ *      MatchSystem
+ */
+public class ServiceController {
+}

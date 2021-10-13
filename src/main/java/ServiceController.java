faec0ce8056f4b817597c0df2e07d643ab603305
hMarc16 <mark.hu@mail.utoronto.ca>
2021-10-13 15:50:10 -0400

    Added methods checking if the input string is numeric. Added exception to isBinary and isNumeric.

--- src/main/java/ServiceController.java
+++ src/main/java/ServiceController.java
@@ -32,11 +32,36 @@ public class ServiceController {
     /**
      * Checks if the input is binary(yes or no)
      * @param input     Input from the user
-     * @return boolean that indicates if the input is indicating yes/no
+     * @return boolean that indicates if the input is indicating yes/no. Exception
+     *         if the string is empty or blank.
      */
-    public static boolean isBinary(String input){
+    public static boolean isBinary(String input) throws Exception{
         String convertedInput = input.toLowerCase();
-        return convertedInput.contains("yes") || convertedInput.contains("no");
+        if (input.isEmpty() || input.isBlank()){
+            throw new Exception("Your input is invalid");
+        } else {
+            return convertedInput.contains("yes") || convertedInput.contains("no");
+        }
+    }
+
+
+    /**
+     * Checks if the input is numeric
+     * @param input     Input from the user
+     * @return boolean that indicates if the input is numeric. Exception if the
+     *         string is empty or blank.
+     */
+    public static boolean isNumeric(String input) throws Exception{
+        if (input.isEmpty() || input.isBlank()){
+            throw new Exception("Your input is invalid");
+        } else {
+            try {
+                double d = Double.parseDouble(input);
+            } catch (NumberFormatException nfe) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }

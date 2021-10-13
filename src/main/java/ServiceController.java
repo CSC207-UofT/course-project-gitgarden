94bbde1f6f7abdce3db0b5f85ca277e962a2696e
hMarc16 <mark.hu@mail.utoronto.ca>
2021-10-13 17:47:10 -0400

    Added the methods for UserInterface to call. Made changes to the existing three methods(removed throw exception from them).

--- src/main/java/ServiceController.java
+++ src/main/java/ServiceController.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 /** ServiceController
  * Responsibilities:
@@ -10,22 +10,66 @@ import java.util.Scanner;
  *      UserInterface
  *      ProfileManager
  *      MatchSystem
+ *
+ * TODO: maybe an exception class for all the exceptions in the program
  */
+
 public class ServiceController {
+
+
+    /**
+     * Method for UserInterface, check if the input is a binary answer
+     * @param ans   Input from the user
+     * @return      Boolean indicating if the user has the correct binary input
+     */
+    public static boolean isYesNoAnswer(String ans) throws Exception{
+        if (isBinary(ans)){
+            return true;
+        } else {
+            throw new Exception("Your input is not valid!");
+        }
+    }
+
+
+    /**
+     * Method for UserInterface, pass parameters to ProfileManager if the input is valid
+     * @param name  Name of the user
+     * @param address   Address of the user
+     * TODO: return type based on the method in Profile Manager
+     */
+    public static void createProfile(String name, String address) throws Exception{
+        if (isAlphanumeric(name)){
+            //TODO: Call the method in ProfileManager
+        } else {
+            throw new Exception("Your input is not valid!");
+        }
+    }
+
+
+    /**
+     * Method for UserInterface, pass parameters to ProfileManager if the input is valid
+     * @param product   Name of the product
+     * @param quantity  Quantity of the product
+     * @param price     Price of the product
+     * TODO: return type based on the method in Profile Manager
+     */
+    public static void createRequest(String product, String quantity, String price)
+            throws Exception{
+        if (isAlphanumeric(product) && isNumeric(quantity) && isNumeric(price)){
+            //TODO: Call the method in ProfileManager
+        } else {
+            throw new Exception("Your input is not valid!");
+        }
+    }
 
 
     /**
      * Checks if the input is alphanumeric.
      * @param input     Input to the program
      * @return boolean that indicates if the input is alphanumeric.
-     * TODO: maybe an exception class for all the exceptions in the program
      */
-    public static boolean isAlphanumeric(String input) throws Exception{
-        if (!input.matches("^[a-zA-Z0-9]*$")){
-            throw new Exception("Your input is invalid");
-        } else {
-            return true;
-        }
+    public static boolean isAlphanumeric(String input){
+        return input.matches("^[a-zA-Z0-9]*$");
     }
 
 
@@ -35,10 +79,10 @@ public class ServiceController {
      * @return boolean that indicates if the input is indicating yes/no. Exception
      *         if the string is empty or blank.
      */
-    public static boolean isBinary(String input) throws Exception{
+    public static boolean isBinary(String input){
         String convertedInput = input.toLowerCase();
         if (input.isEmpty() || input.isBlank()){
-            throw new Exception("Your input is invalid");
+            return false;
         } else {
             return convertedInput.contains("yes") || convertedInput.contains("no");
         }
@@ -51,9 +95,9 @@ public class ServiceController {
      * @return boolean that indicates if the input is numeric. Exception if the
      *         string is empty or blank.
      */
-    public static boolean isNumeric(String input) throws Exception{
+    public static boolean isNumeric(String input){
         if (input.isEmpty() || input.isBlank()){
-            throw new Exception("Your input is invalid");
+            return false;
         } else {
             try {
                 double d = Double.parseDouble(input);

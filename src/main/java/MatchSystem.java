50592a433be1b805f4eed78b600a693db253ceeb
onyxc <onyx.christopher@mail.utoronto.ca>
2021-10-13 12:39:49 -0400

    First MatchSystem commit pending completion of collaborators

---
+++ src/main/java/MatchSystem.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * MatchSystem - Onyx
+ * Class name: MatchSystem
+ * Parent Class: None
+ * Use Case
+ *
+ * Responsibilities:
+ * * Takes input of a single request
+ * * Return a list of recommended distributors, based on the given farmer's preferences
+ * * Receive distributor info and output a list of recommended farmers [expansion]
+ * * Create offer using distributor's information [expansion]
+ *
+ * Collaborators:
+ *  Farmer
+ *  Distributor
+ *  RankingSystem
+ *  ProfileManager
+ *  Request
+ *  Offer
+ *  ServiceController
+ */
+
+public class MatchSystem{
+    public static ArrayList<Distributor> distributors;
+
+    /**
+     *
+     * @param distributors ArrayList of all potential distributors
+     * @param req_name Name of product in request
+     * @param req_price Price per kg of product in request
+     * @return All distributors fulfilling requested criteria
+     */
+    public static ArrayList<Distributor> match(ArrayList<Distributor> distributors, String req_name, double req_price){
+        ArrayList<Distributor> match_list = new ArrayList<>();
+        for(Distributor dist: distributors){
+            if (dist.getProdMap().containsKey(req_name)){
+                if (dist.getProdMap().get(req_name) >= req_price){
+                    match_list.add(dist);
+                }
+
+            }
+        }
+        return match_list;
+    }
+}

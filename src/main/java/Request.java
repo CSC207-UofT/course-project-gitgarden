49e77f99624c67e2fe6c0439493bb7c71754fc01
Jagat Aggarwal <j.aggarwal@mail.utoronto.ca>
2021-10-14 17:13:30 -0400

    Done

---
+++ src/main/java/Request.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Request {
+
+    private String FarmerName;
+    private String FarmerAddress;
+    private String ProductName;
+    private int ProductQuantity;
+    private float ProductPrice;
+    private ArrayList<Offer> Offer;
+
+    public Request(String FName, String FAddress, String PName, int PQuantity,
+                   float PPrice, ArrayList<Offer> Offers) {
+
+        FarmerName = FName;
+        FarmerAddress = FAddress;
+        ProductName = PName;
+        ProductQuantity = PQuantity;
+        ProductPrice = PPrice;
+        Offer = Offers;
+    }
+
+}

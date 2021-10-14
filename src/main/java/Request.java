6c2be0daca5260c8f4d601db90c60bf66181392f
Jagat Aggarwal <j.aggarwal@mail.utoronto.ca>
2021-10-14 17:18:42 -0400

    Commented Offers and removed from the constructor

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -7,17 +7,17 @@ class Request {
     private String ProductName;
     private int ProductQuantity;
     private float ProductPrice;
-    private ArrayList<Offer> Offer;
+    // private ArrayList<Offer> Offer;
 
     public Request(String FName, String FAddress, String PName, int PQuantity,
-                   float PPrice, ArrayList<Offer> Offers) {
+                   float PPrice) {
 
         FarmerName = FName;
         FarmerAddress = FAddress;
         ProductName = PName;
         ProductQuantity = PQuantity;
         ProductPrice = PPrice;
-        Offer = Offers;
+        // Offer = Offers;
     }
 
 }

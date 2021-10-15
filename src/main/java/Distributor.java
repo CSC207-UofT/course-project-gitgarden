eb1fed3f7ad8bd1d7eebb67fa553b0528bba4624
AndyWang-JM <jming.wang@mail.utoronto.ca>
2021-10-15 16:48:31 -0400

    Changed Request and User Class

--- src/main/java/Distributor.java
+++ src/main/java/Distributor.java
@@ -20,10 +20,13 @@ import java.util.ArrayList;
 public class Distributor extends User {
 
     //Initialize list of requests
-    private ArrayList<Request> requests;
+    /**
+    Modification methods of request and proMap will be added later, set to be final for now.
+     */
+    private final ArrayList<Request> requests;
 
     //Initialize prodMap
-    private HashMap<String, Double> prodMap;
+    private final HashMap<String, Double> prodMap;
 
 
     public Distributor(String distributor_name, String distributor_address, HashMap<String, Double> hashmap){
--- src/test/java/FarmerTest.java
+++ src/test/java/FarmerTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 

757fca9216e2b83fddcebeca7bddec20ea8df87a
onyxc <onyx.christopher@mail.utoronto.ca>
2021-10-29 18:52:01 -0400

    Pref first commit

--- src/main/java/Entities/Distributor.java
+++ src/main/java/Entities/Distributor.java
@@ -8,19 +8,49 @@ import java.util.ArrayList;
 public class Distributor extends User {
 
     //Initialize list of requests
-    /**
-    Modification methods of request and proMap will be added later, set to be final for now.
-     */
-    private final ArrayList<Request> requests;
+
+    private ArrayList<Request> requests;
 
     //Initialize prodMap
-    private final HashMap<String, Double> prodMap;
+    private HashMap<String, Double> prodMap;
+
+    private int exposure; // TODO: what is the hard metric
+    private int speed; // Average number of hours in which transactions are completed after being finalized
+    private int carbon; // Number of grams CO2eq emitted per transaction
 
 
     public Distributor(String distributor_name, String distributor_address, HashMap<String, Double> hashmap){
         super(distributor_name, distributor_address);
         this.requests = null;
         this.prodMap = hashmap;
+    }
+
+    public void setExposure(int exposure) {
+        this.exposure = exposure;
+    }
+
+    public int getExposure() {
+        return exposure;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setCarbon(int carbon) {
+        this.carbon = carbon;
+    }
+
+    public int getCarbon() {
+        return carbon;
+    }
+
+    public void addRequest(ArrayList<Request> requests) {
+        this.requests = requests;
     }
 
     public ArrayList<Request> getRequests(){
--- src/main/java/Entities/Farmer.java
+++ src/main/java/Entities/Farmer.java
@@ -15,13 +15,50 @@ public class Farmer extends User {
     private final ArrayList<Request> requests;
     //I am using array to show list of request for now, but depending on request's structure, it could be changed later
 
+    private int pref_price; // Int from 0-10 detailing preference for exact or better price (10 is rigid price)
+    private int pref_exposure; // Int from 0-10 detailing preference for high exposure (10 is maximum exposure)
+    private int pref_speed; // Int from 0-10 detailing preference for fast pickup (10 is fastest pickup)
+    private int pref_carbon; // Int from 0-10 detailing preference for low-carbon distribution (10 is lowest emissions)
+
     public Farmer(String farmer_name, String farmer_address){
         super(farmer_name, farmer_address);
         this.requests = null;
     }
 
-    public ArrayList<Request> getRequests() {
+    public void setPref_price(int pref_price) {
+        this.pref_price = pref_price;
+    }
+
+    public void setPref_exposure(int pref_exposure) {
+        this.pref_exposure = pref_exposure;
+    }
+
+    public void setPref_speed(int pref_speed) {
+        this.pref_speed = pref_speed;
+    }
+
+    public void setPref_carbon(int pref_carbon) {
+        this.pref_carbon = pref_carbon;
+    }
+
+    public ArrayList<Request> getRequests(){
         return requests;
+    }
+
+    public int getPref_price(){
+        return pref_price;
+    }
+
+    public int getPref_exposure(){
+        return pref_exposure;
+    }
+
+    public int getPref_speed(){
+        return pref_speed;
+    }
+
+    public int getPref_carbon(){
+        return pref_carbon;
     }
 
 }

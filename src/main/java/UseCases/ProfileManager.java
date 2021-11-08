3d05f63a920790f5021079efebde44abd988b109
DivitSingh <divit.singh@mail.utoronto.ca>
2021-11-08 16:51:18 -0500

    GSON Added.

--- src/main/java/UseCases/ProfileManager.java
+++ src/main/java/UseCases/ProfileManager.java
@@ -1,18 +1,18 @@
 package UseCases;
 
 import Entities.Distributor;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ProfileManager {
     //public static ArrayList<User> farmerList = new ArrayList<>();
+    Gson gson = new Gson();
     public static ArrayList<Distributor> distributorList = new ArrayList<>();
 
     public static void createDistributor(String name, String address, HashMap<String, Double> prodMap){
         Distributor dist = new Distributor(name, address, prodMap);
         distributorList.add(dist);
-
     }
-
 }

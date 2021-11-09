ec2c35d337e92941260673f10ec35617626c0b22
hMarc16 <mark.hu@mail.utoronto.ca>
2021-11-09 17:18:34 -0500

    Added main in ProfileManager and convert distributorList to json format.

--- src/main/java/UseCases/ProfileManager.java
+++ src/main/java/UseCases/ProfileManager.java
@@ -3,16 +3,31 @@ package UseCases;
 import Entities.Distributor;
 import com.google.gson.Gson;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ProfileManager {
     //public static ArrayList<User> farmerList = new ArrayList<>();
-    Gson gson = new Gson();
     public static ArrayList<Distributor> distributorList = new ArrayList<>();
 
     public static void createDistributor(String name, String address, HashMap<String, Double> prodMap){
         Distributor dist = new Distributor(name, address, prodMap);
         distributorList.add(dist);
+    }
+
+    public static void main(String[] args) throws IOException {
+        //Created these two distributors for testing purposes.
+        createDistributor("Mark","Toronto", null);
+        createDistributor("Divit","Toronto", null);
+
+        //This will convert distributorList to a json file.
+        Gson gson = new Gson();
+        System.out.println(distributorList);
+
+        String json = gson.toJson(distributorList);
+        System.out.println(json);
+
     }
 }

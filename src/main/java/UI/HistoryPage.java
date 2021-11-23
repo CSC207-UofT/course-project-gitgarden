2652eb8bfa15524b0f4254cf3ab1d696dfe2fafe
Patrick <1400115@dishs.tp.edu.tw>
2021-11-23 18:01:36 -0500

    added HistoryPage clicker in FarmerPage, and finished HistoryPage

--- src/main/java/UI/HistoryPage.java
+++ src/main/java/UI/HistoryPage.java
@@ -1,8 +1,15 @@
 package UI;
 
-import javax.swing.*;
+import Controller.DataPresenter;
+import Controller.IFetch;
 
-public class HistoryPage {
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HistoryPage extends JFrame{
     private JPanel mainPanel;
     private JPanel titlePanel;
     private JLabel titleText;
@@ -10,8 +17,43 @@ public class HistoryPage {
     private JPanel detailsTextPanel;
     private JLabel detailsText;
     private JPanel detailsListPanel;
-    private JList detailsList;
+    private JList<String> detailsList;
     private JPanel buttonPanel;
     private JPanel closeButtonPanel;
     private JButton closeButton;
+
+    private final IFetch presenter = new DataPresenter();
+    public HistoryPage(String request){
+        setTitle("farmerPage");
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,700);
+
+        DefaultListModel<String> listModel= new DefaultListModel<String>();
+        String[] info = presenter.fetchRequestInformation(request);
+
+        listModel.addElement("Request ID: " + request);
+        listModel.addElement("Product name: " + info[0]);
+        listModel.addElement("Farmer name: " + info[3]);
+        listModel.addElement("Farmer address: " + presenter.fetchUserAddress(WelcomePage.currUserId));
+        listModel.addElement("Product Quantity: " + info[1]);
+        listModel.addElement("Product Price: " + info[2]);
+
+        detailsList.setModel(listModel);
+
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                if(WelcomePage.flag){
+                    JFrame farmerPage = new FarmerPage();
+                    farmerPage.setVisible(true);
+                }
+                else{
+                    JFrame distributorPage = new DistributorPage();
+                    distributorPage.setVisible(true);
+                }
+            }
+        });
+    }
 }

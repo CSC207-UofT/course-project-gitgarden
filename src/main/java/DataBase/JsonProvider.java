81d2d427fa2d48ca81c8f3932138eac8c4c969b5
hMarc16 <mark.hu@mail.utoronto.ca>
2021-11-26 13:25:13 -0500

    Created DataBase. Created JsonProvider in DataBase which implements the DataAccessInterface.

---
+++ src/main/java/DataBase/JsonProvider.java
@@ -0,0 +1,36 @@
+package DataBase;
+
+import UseCases.DataAccessInterface;
+
+import Entities.Distributor;
+import Entities.Farmer;
+import Entities.Request;
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JsonProvider implements DataAccessInterface {
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void write() {
+
+    }
+
+    @Override
+    public void loadFarmer() {
+
+    }
+
+    @Override
+    public void loadDistributor() {
+
+    }
+}

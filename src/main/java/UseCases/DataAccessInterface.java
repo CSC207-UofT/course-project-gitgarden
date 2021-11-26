85d0f8be6e932003d272dae27098df1cccd5be52
hMarc16 <mark.hu@mail.utoronto.ca>
2021-11-26 13:04:52 -0500

    Created DataAccessInterface in User Cases for persistency.

---
+++ src/main/java/UseCases/DataAccessInterface.java
@@ -0,0 +1,11 @@
+package UseCases;
+
+import Entities.Distributor;
+import Entities.Farmer;
+import Entities.User;
+import Entities.Request;
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+
+public interface DataAccessInterface {
+}

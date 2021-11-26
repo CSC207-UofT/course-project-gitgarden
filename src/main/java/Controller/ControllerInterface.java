879ef53881d9cf520857d43510a9fe190b5a7320
Patrick <1400115@dishs.tp.edu.tw>
2021-11-26 17:46:36 -0500

    ControllerInterface and service controllre changed so that they can throw errors now.

--- src/main/java/Controller/ControllerInterface.java
+++ src/main/java/Controller/ControllerInterface.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public interface ControllerInterface {
 
-    String createProfileCheck(String name, String address, boolean flag);
+    String createProfileCheck(String name, String address, boolean flag) throws Exception;
 
-    void modifyUserCheck(String Userid, String newName, String address);
+    void modifyUserCheck(String Userid, String newName, String address) throws Exception;
 
     void modifyFarmerCheck(String FarmerId, double slider1, double slider2, double slider3, double slider4);
 
     void modifyDistributorCheck(String distId, double slider2, double slider3, double slider4);
 
-    void createRequestCheck(String userId, String product, String quantity, String price);
+    void createRequestCheck(String userId, String product, String quantity, String price) throws Exception;
 
-    void createCounterOfferCheck(String userId, String requestId, String quantity, String price);
+    void createCounterOfferCheck(String userId, String requestId, String quantity, String price) throws Exception;
 
     void acceptRequestCheck(String requestId, String userId);
 

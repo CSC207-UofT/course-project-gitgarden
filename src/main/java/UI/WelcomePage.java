3725084c05eb7d5d1bbc044580f3ce3c7ba28303
Patrick <1400115@dishs.tp.edu.tw>
2021-12-07 02:13:44 -0500

    Modified WelcomePage, controller and presenter are now injected. Also did some refactoring

--- src/main/java/UI/WelcomePage.java
+++ src/main/java/UI/WelcomePage.java
@@ -66,96 +66,77 @@ public class WelcomePage extends JFrame{
     public static Boolean flag;
     public static String currUserId = "";
     public static Boolean dark = false;
-    private final ControllerInterface sc = new ServiceController();
-    private final IFetch presenter = new DataPresenter();
     private final JPanel[] panelList = {titlePanel, signinPanel, textPanel, inputPanel, signinButtonPanel, sinupPanel,
             namePanel, pricePanel, preferencePanel, prefInputPanel, nameInputPanel, priceInputPanel, textsPanel,
             inputsPanel, separatePanel, farmerPanel, choicePanel, buttonPanel, userTextPanel, distributorPanel,
             mainPanel, priceTextPanel, exposureTextPanel, speedTextPanel, carbonTextPanel, pricePrePanel,
             exposurePrePanel, speedPrePanel, carbonPrePanel};
-    public WelcomePage() {
+    public WelcomePage(ControllerInterface controller, IFetch presenter) {
         setContentPane(mainPanel);
         setTitle("Welcome");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
         setSize(800, 700);
 
-        signInButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String username = nameInput.getText();
+        signInButton.addActionListener(e -> {
+            String username = nameInput.getText();
 
-                if (username.equals("")) {
-                    JOptionPane.showMessageDialog(null,"Please enter your User Name");
-                    newUserName.requestFocusInWindow();
+            if (username.equals("")) {
+                JOptionPane.showMessageDialog(null,"Please enter your User Name");
+                newUserName.requestFocusInWindow();
+            }
+            else {
+                if (presenter.fetchAllFarmerNames().contains(username)){
+                    currUserId = presenter.fetchUserId(username);
+                    flag = true;
+                    FarmerPage farmerPage = new FarmerPage(controller, presenter);
+                    setVisible(false);
+                    farmerPage.setVisible(true);
+                }
+                else if (presenter.fetchAllDistNames().contains(username)){
+                    currUserId = presenter.fetchUserId(username);
+                    flag = false;
+                    DistributorPage distributorPage = new DistributorPage(controller,presenter);
+                    setVisible(false);
+                    distributorPage.setVisible(true);
                 }
                 else {
-                    if (presenter.fetchAllFarmerNames().contains(username)){
-                        currUserId = presenter.fetchUserId(username);
-                        flag = true;
-                        FarmerPage farmerPage = new FarmerPage();
+                    JOptionPane.showMessageDialog(null, "Please enter a valid User Name or " +
+                            "Create a new Profile");
+                    newUserName.requestFocusInWindow();
+                }
+            }
+        });
+        farmerButton.addActionListener(e -> flag = true);
+        distributorButton.addActionListener(e -> flag = false);
+        signupButton.addActionListener(e -> {
+            String address = addressInput.getText();
+            double slider1_value = slider1.getValue();
+            double slider2_value = slider2.getValue();
+            double slider3_value = slider3.getValue();
+            double slider4_value = slider4.getValue();
+            String name = newUserName.getText();
+            if (flag == null) {
+                JOptionPane.showMessageDialog(null,"Please choose farmer or distributor");
+            }
+            else{
+                try{
+                    currUserId = controller.createProfileCheck(name,address, flag);
+                    if (flag) {
+                        controller.modifyFarmerCheck(currUserId, slider1_value, slider2_value, slider3_value, slider4_value);
+                        FarmerPage farmerPage = new FarmerPage(controller, presenter);
                         setVisible(false);
                         farmerPage.setVisible(true);
-                    }
-                    else if (presenter.fetchAllDistNames().contains(username)){
-                        currUserId = presenter.fetchUserId(username);
-                        flag = false;
-                        DistributorPage distributorPage = new DistributorPage();
+                        setContentPane(new FarmerPage(controller, presenter).mainPanel);
+                    } else {
+                        controller.modifyDistributorCheck(currUserId, slider2_value, slider3_value, slider4_value);
+                        DistributorPage distributorPage = new DistributorPage(controller, presenter);
                         setVisible(false);
                         distributorPage.setVisible(true);
-                    }
-                    else {
-                        JOptionPane.showMessageDialog(null, "Please enter a valid User Name or " +
-                                "Create a new Profile");
-                        newUserName.requestFocusInWindow();
+                        setContentPane(new DistributorPage(controller, presenter).mainPanel);
                     }
                 }
-            }
-        });
-        farmerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                flag = true;
-            }
-        });
-        distributorButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                flag = false;
-            }
-        });
-        signupButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String address = addressInput.getText();
-                double slider1_value = slider1.getValue();
-                double slider2_value = slider2.getValue();
-                double slider3_value = slider3.getValue();
-                double slider4_value = slider4.getValue();
-                String name = newUserName.getText();
-                if (flag == null) {
-                    JOptionPane.showMessageDialog(null,"Please choose farmer or distributor");
-                }
-                else{
-                    try{
-                        currUserId = sc.createProfileCheck(name,address, flag);
-                        if (flag) {
-                            sc.modifyFarmerCheck(currUserId, slider1_value, slider2_value, slider3_value, slider4_value);
-                            FarmerPage farmerPage = new FarmerPage();
-                            setVisible(false);
-                            farmerPage.setVisible(true);
-                            setContentPane(new FarmerPage().mainPanel);
-                        } else {
-                            sc.modifyDistributorCheck(currUserId, slider2_value, slider3_value, slider4_value);
-                            DistributorPage distributorPage = new DistributorPage();
-                            setVisible(false);
-                            distributorPage.setVisible(true);
-                            setContentPane(new DistributorPage().mainPanel);
-                        }
-                    }
-                    catch (Exception profileException){
-                        JOptionPane.showMessageDialog(null,profileException.getMessage());
-                    }
+                catch (Exception profileException){
+                    JOptionPane.showMessageDialog(null,profileException.getMessage());
                 }
             }
         });
@@ -168,46 +149,20 @@ public class WelcomePage extends JFrame{
         slider4.setPaintTicks(true);
         slider4.setMinorTickSpacing(10);
 
-        darkCheckBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (darkCheckBox.isSelected()){
-                    dark = true;
-                    for (JPanel p : panelList) {
-                        p.setBackground(new Color(0x011627));
-                    }
+        darkCheckBox.addActionListener(e -> {
+            if (darkCheckBox.isSelected()){
+                dark = true;
+                for (JPanel p : panelList) {
+                    p.setBackground(new Color(0x011627));
                 }
-                else{
-                    dark = false;
-                    for (JPanel p : panelList) {
-                        p.setBackground(new Color(0x4C566A));
-                    }
+            }
+            else{
+                dark = false;
+                for (JPanel p : panelList) {
+                    p.setBackground(new Color(0x4C566A));
                 }
             }
         });
 
-    }
-
-
-    public static void main(String[] args){
-        try {
-            JsonProvider jp = new JsonProvider();
-            jp.loadFarmer("farmers.json");
-            jp.loadDistributor("distributors.json");
-            jp.modifyFarmer("farmerMod.json");
-            jp.modifyDistributor("distMod.json");
-            jp.loadRequests("requests.json");
-            jp.loadCounterOffers("counters.json");
-        } catch (FileNotFoundException e){
-            System.out.println("First time running, there's nothing to be read yet!");
-        }
-
-        WelcomePage welcomePage = new WelcomePage();
-        welcomePage.setVisible(true);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            JsonProvider jp = new JsonProvider();
-            jp.writeUsers();
-        }));
     }
 }

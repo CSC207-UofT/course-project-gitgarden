84081b073dbcd6b29bfb9c5b93fcf37073520807
Patrick <1400115@dishs.tp.edu.tw>
2021-12-07 02:14:17 -0500

    Modified RequestPage, controller and presenter are now injected. Also did some refactoring

--- src/main/java/UI/RequestPage.java
+++ src/main/java/UI/RequestPage.java
@@ -1,6 +1,7 @@
 package UI;
 
 import Controller.ControllerInterface;
+import Controller.IFetch;
 import Controller.ServiceController;
 
 import javax.swing.*;
@@ -29,39 +30,37 @@ public class RequestPage extends JFrame{
     private JTextField quantityInput;
     private JTextField priceInput;
     private JButton createButton;
-    private final ControllerInterface sc = new ServiceController();
-    private final JPanel[] panelList = {mainPanel, titlePanel, middlePanel, buttonPanel, textPanel, inputPanel,
-                                        namePanel, quanInputPanel, pricePanel, nameInputPanel, quantityPanel,
-                                        priceInputPanel};
+    private final ControllerInterface sc;
 
 
-    public RequestPage(){
+    public RequestPage(ControllerInterface controller, IFetch presenter){
+        sc = controller;
         setTitle("requestPage");
         setContentPane(mainPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800,700);
-        createButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String product = ProductNameInput.getText();
-                String quantity = quantityInput.getText();
-                String price = priceInput.getText();
-                if (product.equals("")|| quantity.equals("")|| price.equals("")) {
-                    JOptionPane.showMessageDialog(null,"Please enter all information");
-                }
-                try{
-                    sc.createRequestCheck(WelcomePage.currUserId, product, quantity, price);
-                    JFrame profile = new FarmerPage();
-                    setVisible(false);
-                    profile.setVisible(true);
-                }
-                catch(Exception requestException){
-                    JOptionPane.showMessageDialog(null,requestException.getMessage());
-                }
+        createButton.addActionListener(e -> {
+            String product = ProductNameInput.getText();
+            String quantity = quantityInput.getText();
+            String price = priceInput.getText();
+            if (product.equals("")|| quantity.equals("")|| price.equals("")) {
+                JOptionPane.showMessageDialog(null,"Please enter all information");
+            }
+            try{
+                sc.createRequestCheck(WelcomePage.currUserId, product, quantity, price);
+                JFrame profile = new FarmerPage(controller, presenter);
+                setVisible(false);
+                profile.setVisible(true);
+            }
+            catch(Exception requestException){
+                JOptionPane.showMessageDialog(null,requestException.getMessage());
             }
         });
 
         if (WelcomePage.dark){
+            JPanel[] panelList = {mainPanel, titlePanel, middlePanel, buttonPanel, textPanel, inputPanel,
+                    namePanel, quanInputPanel, pricePanel, nameInputPanel, quantityPanel,
+                    priceInputPanel};
             for (JPanel p : panelList) {
                 p.setBackground(new Color(0x011627));
             }

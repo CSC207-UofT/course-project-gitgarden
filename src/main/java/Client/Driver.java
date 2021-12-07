4f31011ee6a9a444944b6d8a45a3ce8fe933d814
Patrick <1400115@dishs.tp.edu.tw>
2021-12-07 02:29:53 -0500

    Added Facade interface, renamed original facade to instantiater. Made corresponding changes to driver

--- src/main/java/Client/Driver.java
+++ src/main/java/Client/Driver.java
@@ -2,6 +2,7 @@ package Client;
 
 public class Driver {
     public static void main(String[] args) {
-        Facade.build();
+        Facade instantiater = new Instantiater();
+        instantiater.build();
     }
 }
--- src/main/java/Client/Facade.java
+++ src/main/java/Client/Facade.java
@@ -1,40 +1,5 @@
 package Client;
 
-import Controller.ControllerInterface;
-import Controller.DataPresenter;
-import Controller.IFetch;
-import Controller.ServiceController;
-import DataBase.JsonProvider;
-import Client.UI.WelcomePage;
-import UseCases.ProfileInterface;
-import UseCases.ProfileManager;
-import UseCases.RequestInterface;
-import UseCases.RequestManager;
-
-import java.io.FileNotFoundException;
-
-public class Facade {
-    public static void build(){
-        try {
-            JsonProvider jp = new JsonProvider();
-            jp.loadFarmer("farmers.json");
-            jp.loadDistributor("distributors.json");
-            jp.modifyFarmer("farmerMod.json");
-            jp.modifyDistributor("distMod.json");
-            jp.loadRequests("requests.json");
-            jp.loadCounterOffers("counters.json");
-        } catch (FileNotFoundException e){
-            System.out.println("First time running, there's nothing to be read yet!");
-        }
-        ProfileInterface pm = new ProfileManager();
-        RequestInterface rm = new RequestManager();
-        ControllerInterface sc = new ServiceController(pm, rm);
-        IFetch dp = new DataPresenter(pm, rm);
-        WelcomePage welcomePage = new WelcomePage(sc, dp);
-        welcomePage.setVisible(true);
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            JsonProvider jp = new JsonProvider();
-            jp.writeUsers();
-        }));
-    }
+public interface Facade {
+    void build();
 }

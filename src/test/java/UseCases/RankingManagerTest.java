3bd9689bce8256636677726c247faccae8176e77
AndyWang-JM <jming.wang@mail.utoronto.ca>
2021-12-08 08:30:40 -0500

    Added all testing required

--- src/test/java/UseCases/RankingManagerTest.java
+++ src/test/java/UseCases/RankingManagerTest.java
@@ -1,4 +1,5 @@
 package UseCases;
+import Controller.ServiceController;
 import Entities.*;
 import UseCases.RankingManager;
 
@@ -10,48 +11,43 @@ import java.util.ArrayList;
 import static java.lang.Math.*;
 
     public class RankingManagerTest {
-        RankingManager rm;
+        ServiceController sc;
+        ProfileManager pm;
+        RequestManager rm;
+        RankingManager rkm;
 
         @Before
         public void setUp(){
-            ArrayList<IDistributor> allDist= new ArrayList<>();
-            IDistributor a = new Distributor("Onyx", "A", 1);
-            IDistributor b = new Distributor("Andy", "B", 2);
-            IDistributor c = new Distributor("Patrick", "C", 3);
-            IDistributor d = new Distributor("Mark", "D", 4);
-            IDistributor e = new Distributor("Divit", "E", 5);
-            IDistributor f = new Distributor("Melaney", "F", 6);
-            IDistributor g = new Distributor("Jagat", "G", 7);
+            pm = new ProfileManager();
+            rm = new RequestManager();
+            pm.createFarmer("Pooria", "H", 8);
+            pm.createDistributor("Onyx", "A", 1);
+            pm.createDistributor("Andy", "B", 2);
+            pm.createDistributor("Patrick", "C", 3);
+            pm.createDistributor("Mark", "D", 4);
+            pm.createDistributor("Divit", "E", 5);
+            pm.createDistributor("Melaney", "F", 6);
+            pm.createDistributor("Jagat", "G", 7);
+            rm.createRequest(0, "8", "Orange", 50d, 3.00);
 
-            allDist.add(a);
-            allDist.add(b);
-            allDist.add(c);
-            allDist.add(d);
-            allDist.add(e);
-            allDist.add(f);
-            allDist.add(g);
-
-            rm = new RankingManager("0", "1");
-            Request offer = new Request(0, "Orange", 50, 3.00, null);
-            for (int i = 0; i < allDist.size(); i++){
-                allDist.get(i).addOffer(offer);
-                allDist.get(i).setExposure(1 + abs(i - 5));
-                allDist.get(i).setSpeed(30 - 2 * i);
-                allDist.get(i).setCarbon(5 + 10 * i);
+            for (int i = 0; i < pm.getDistributorList().size(); i++){
+                pm.getDistributorList().get(i).addOffer(rm.getRequestFromId("0"));
+                pm.getDistributorList().get(i).setExposure(1 + abs(i - 5));
+                pm.getDistributorList().get(i).setSpeed(30 - 2 * i);
+                pm.getDistributorList().get(i).setCarbon(5 + 10 * i);
             }
-            IFarmer farmer = new Farmer("Pooria", "H", 8);
-            farmer.setPrefPrice(0);
-            farmer.setPrefExposure(0);
-            farmer.setPrefSpeed(0);
-            farmer.setPrefCarbon(0);
 
-            this.rm = new RankingManager("0", "1");
+            pm.modifyFarmer("8", 0, 0, 0, 0);
+
+            rkm = new RankingManager("0", "1");
+            sc = new ServiceController(pm, rm);
         }
 
 //Functionalities
 
         @Test
         public void rankDistributors(){
+            System.out.println(sc.rank("0", 1));
         }
 
     }

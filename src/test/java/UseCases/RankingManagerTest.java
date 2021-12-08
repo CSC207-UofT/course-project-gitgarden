4ab57d74dc62fd90de6b879d2c7db3b29bf6c26d
AndyWang-JM <jming.wang@mail.utoronto.ca>
2021-12-08 08:31:39 -0500

    Added all testing required

--- src/test/java/UseCases/RankingManagerTest.java
+++ src/test/java/UseCases/RankingManagerTest.java
@@ -47,7 +47,6 @@ import static java.lang.Math.*;
 
         @Test
         public void rankDistributors(){
-            System.out.println(sc.rank("0", 1));
+            System.out.println(sc.rank("0", "1"));
         }
-
     }
